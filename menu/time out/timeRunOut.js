3ee30e36a7b2deb89052dff85c1e3b0a428eb3a8
yasirad <101118594+yasirad@users.noreply.github.com>
2022-06-07 00:30:54 +0200

    transition screens
    Menu page. Time out page. Moving to next level page. End of game page. Doesnt include music or the pages its referring to .

---
+++ menu/time out/timeRunOut.js
@@ -0,0 +1,60 @@
+
+// 1. Create the button
+var button = document.createElement("button");
+button.style.position = "absolute";
+button.style.top = "0%"; //place in middle
+button.style.left = "0%"; //place in middle
+button.style.fontSize = "20px"
+button.innerHTML = "RE-START LEVEL"; //write text
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+console.log("start switch") //this is where we put code to say stay on same level!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+});
+
+
+// 1. Create the button
+//returning to main menu
+var button1 = document.createElement("button");
+button1.style.position = "absolute";
+button1.style.left = "0%";
+button1.style.top = "30%"; //slghty below 1st button
+button1.style.fontSize = "17px" //size text
+button1.innerHTML = "RETURN TO MAIN MENU"; //text
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button1);
+// 3. Add event handler
+button1.addEventListener ("click", function() {
+     console.log("credits switch") //this is where we put code to say move to menu.js !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+});
+
+
+//instructions  for game
+var instr = document.createElement("caption");
+instr.style.fontSize = "25px";
+instr.innerHTML = "Use your mouse to change the view. Use WASD to move the character. Look for the treasure to escape the maze before time runs out."
+body.appendChild(instr);
+//header 
+var nameGame = document.createElement("h1");
+nameGame.style.position = "absolute";
+nameGame.style.top = "-5%"; //move above
+nameGame.innerHTML = "Oh no, times up!";
+body.appendChild(nameGame);
+//header 2 
+var slow = document.createElement("h1");
+slow.innerHTML = "You were too slow, try again.";
+slow.style.position = "absolute";
+slow.style.top = "15%"; //place below first header
+slow.style.fontSize = "80px";
+body.appendChild(slow);
+
+
+
+//
+document.body.style.backgroundColor = '#101820FF'; //set background colour
